388012d305737f4780dab0530b7265c3a4309e7b
greenjh1999 <122394168+greenjh1999@users.noreply.github.com>
2024-01-02 22:45:32 -0500

    Add files via upload

---
+++ functions/sendRsvp.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = async function (event, context) {
+  const { name, message } = JSON.parse(event.body);
+
+  console.log('Received RSVP data:', { name, message });
+
+  const transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'no_reply_john_green@outlook.com',
+    to: 'johngreenmauhs12345@gmail.com',
+    subject: `New Babyshower RSVP from ${name}`,
+    text: `${message}`,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully:', info);
+    return {
+      statusCode: 200,
+      body: 'Email Sent!',
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  }
+};
