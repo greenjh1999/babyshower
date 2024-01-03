92354ed1a2d8fde1a173caf91c262d18b820d110
greenjh1999 <122394168+greenjh1999@users.noreply.github.com>
2024-01-03 00:03:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite';
+// vite.config.js
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import ViteReactPlugin from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ViteReactPlugin()],
   build: {
-    outDir: 'build',
+    cssCodeSplit: true,
+    sourcemap: true,
+    // Add your purge configuration here
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'everything.js',
+      },
+    },
+    // PurgeCSS configuration
+    // Ensure to include all necessary paths
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+      },
+    },
+    // ...
   },
 });
+
